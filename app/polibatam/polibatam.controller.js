4faac4c6509310d95d5ca2c1ec2de1e2bead387b
dickyd44 <dikydrmwn08@gmail.com>
2022-09-22 20:40:35 +0700

    add isAdmin

--- app/polibatam/polibatam.controller.js
+++ app/polibatam/polibatam.controller.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 const { Ok, InternalServerError } = require("../../utils/http-response");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 module.exports = {
   Login: async (req, res) => {
@@ -28,30 +30,66 @@ module.exports = {
   },
   ReadDosen: async (req, res) => {
     try {
-      await axios({
+      const result = await axios({
         method: "post",
         url:
           process.env.API_POLIBATAM_URL +
           "/tampilkan-semua?pilih=1&token=" +
           process.env.API_POLIBATAM_TOKEN,
-      }).then((response) => {
-        Ok(res, response.data.data, response.data.message);
       });
+
+      const admins = await prisma.admin.findMany();
+
+      const data = [];
+      result.data.data.filter((item) => {
+        const admin = admins.find((admin) => admin.nim == item.nim_nik_unit);
+        if (admin) {
+          data.push({
+            ...item,
+            isAdmin: true,
+          });
+        } else {
+          data.push({
+            ...item,
+            isAdmin: false,
+          });
+        }
+      });
+
+      Ok(res, data, "Berhasil mengambil data");
     } catch (error) {
       InternalServerError(res, {}, error);
     }
   },
   ReadMahasiswa: async (req, res) => {
     try {
-      await axios({
+      const result = await axios({
         method: "post",
         url:
           process.env.API_POLIBATAM_URL +
           "/tampilkan-semua?pilih=0&token=" +
           process.env.API_POLIBATAM_TOKEN,
-      }).then((response) => {
-        Ok(res, response.data.data, response.data.message);
       });
+
+      const admins = await prisma.admin.findMany();
+
+      const data = [];
+      result.data.data.filter((item) => {
+        const admin = admins.find((admin) => admin.nim == item.nim_nik_unit);
+        if (admin) {
+          data.push({
+            ...item,
+            isAdmin: true,
+          });
+        } else {
+          data.push({
+            ...item,
+            isAdmin: false,
+          });
+        }
+      });
+
+      Ok(res, data, "Berhasil mengambil data");
     } catch (error) {
       InternalServerError(res, {}, error);
     }
@@ -64,6 +102,41 @@ module.exports = {
       }).then((response) => {
         Ok(res, response.data.data, response.data.message);
       });
+    } catch (error) {
+      InternalServerError(res, {}, error);
+    }
+  },
+  SetIsAdmin: async (req, res) => {
+    try {
+      const check = await prisma.admin.findUnique({
+        where: {
+          nim: req.params.nim,
+        },
+      });
+      if (check) {
+        return InternalServerError(res, {}, "User sudah menjadi Admin");
+      }
+
+      const result = await prisma.admin.create({
+        data: {
+          nim: req.params.nim,
+        },
+      });
+
+      Ok(res, result, "Berhasil mengubah status admin");
+    } catch (error) {
+      InternalServerError(res, {}, error);
+    }
+  },
+  UnsetIsAdmin: async (req, res) => {
+    try {
+      const result = await prisma.admin.delete({
+        where: {
+          nim: req.params.nim,
+        },
+      });
+
+      Ok(res, result, "Berhasil mengubah status admin");
     } catch (error) {
       InternalServerError(res, {}, error);
     }
--- app/polibatam/polibatam.router.js
+++ app/polibatam/polibatam.router.js
@@ -5,11 +5,15 @@ const {
   ReadDosen,
   ReadMahasiswa,
   ReadMahasiswaByNim,
+  SetIsAdmin,
+  UnsetIsAdmin,
 } = require("./polibatam.controller");
 
 router.post("/login", Login);
 router.get("/dosen", ReadDosen);
 router.get("/mahasiswa", ReadMahasiswa);
 router.get("/mahasiswa/:nim", ReadMahasiswaByNim);
+router.get("/setAdmin/:nim", SetIsAdmin);
+router.delete("/unsetAdmin/:nim", UnsetIsAdmin);
 
 module.exports = router;
