837699b3e1ddec451265084f23ab1c614e4f6eee
lazlanrafar <lazlanrafar@gmail.com>
2023-02-14 14:35:32 +0700

    setup cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
 require("dotenv").config();
-var app = express();
+const app = express();
+
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
